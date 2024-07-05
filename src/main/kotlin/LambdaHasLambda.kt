2853d0f37053b897d741cf9a0f9c511e150f53b2
Bandal <no1msh1217@gmail.com>
2024-07-05 18:56:11 +0900

    feat: 람다식을 파라미터로 가지는 람다식 정의

---
+++ src/main/kotlin/LambdaHasLambda.kt
@@ -0,0 +1,20 @@
+suspend fun main() {
+    val lambdaHasLambda: suspend ((Int) -> Unit) -> Unit = { emit ->
+        emit(1)
+        emit(2)
+        emit(3)
+    }
+
+    lambdaHasLambda { number: Int ->
+        print(number)
+    }
+
+    println()
+
+    lambdaHasLambda { number: Int ->
+        print(number)
+    }
+}
+
+// 123
+// 123
