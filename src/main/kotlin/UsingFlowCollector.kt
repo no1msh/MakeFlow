7395d40d7af4ebc79594666f9dcd11706fca47cd
Bandal <no1msh1217@gmail.com>
2024-07-05 19:58:04 +0900

    feat: FlowCollector를 사용하는 람다식 정의

---
+++ src/main/kotlin/UsingFlowCollector.kt
@@ -0,0 +1,14 @@
+suspend fun main() {
+    val lambdaHasFlowCollector: suspend (FlowCollector) -> Unit = {
+        it.emit(1)
+        it.emit(2)
+        it.emit(3)
+    }
+
+    lambdaHasFlowCollector { print(it) }
+    println()
+    lambdaHasFlowCollector { print(it) }
+}
+
+// 123
+// 123
