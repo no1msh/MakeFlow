f94d0dc933ac42a1ebdba131433c9f8833da109a
Bandal <no1msh1217@gmail.com>
2024-07-05 20:03:46 +0900

    feat: FlowCollector을 리시버로 만들어 정의

---
+++ src/main/kotlin/UsingReceiverFlowCollector.kt
@@ -0,0 +1,14 @@
+suspend fun main() {
+    val lambdaHasFlowCollector: suspend FlowCollector.() -> Unit = {
+        emit(1)
+        emit(2)
+        emit(3)
+    }
+
+    lambdaHasFlowCollector { print(it) }
+    println()
+    lambdaHasFlowCollector { print(it) }
+}
+
+// 123
+// 123
