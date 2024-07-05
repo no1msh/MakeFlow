87daa3a8ee049918c6cf67cf3c531ec33a9d49d1
Bandal <no1msh1217@gmail.com>
2024-07-05 22:26:49 +0900

    feat: Flow 빌더 사용

---
+++ src/main/kotlin/UsingFlowBuilder.kt
@@ -0,0 +1,14 @@
+suspend fun main() {
+    val flow:Flow = flow {
+        emit(1)
+        emit(2)
+        emit(3)
+    }
+
+    flow.collect { print(it) }
+    println()
+    flow.collect { print(it) }
+}
+
+// 123
+// 123
