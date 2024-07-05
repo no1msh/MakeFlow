8befd38398a2b10e5a3767e117acd2eb0f68cc45
Bandal <no1msh1217@gmail.com>
2024-07-05 18:11:50 +0900

    feat: 중단 가능한 람다식 정의

---
+++ src/main/kotlin/SuspendLambda.kt
@@ -0,0 +1,18 @@
+import kotlinx.coroutines.delay
+
+suspend fun main() {
+    val suspendLambda: suspend () -> Unit = {
+        print(1)
+        delay(1000)
+        print(2)
+        delay(1000)
+        print(3)
+    }
+
+    suspendLambda()
+    println()
+    suspendLambda()
+}
+
+// 1(1초 후)2(1초 후)3
+// 1(1초 후)2(1초 후)3
