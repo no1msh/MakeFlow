5fac6d5110525d6a65daeb1570474255d9896153
Bandal <no1msh1217@gmail.com>
2024-07-05 22:17:49 +0900

    feat: Flow 인터페이스를 사용

---
+++ src/main/kotlin/UsingFlowInterface.kt
@@ -0,0 +1,20 @@
+suspend fun main() {
+    val builder: suspend FlowCollector.() -> Unit = {
+        emit(1)
+        emit(2)
+        emit(3)
+    }
+
+    val flow: Flow = object : Flow {
+        override suspend fun collect(flowCollector: FlowCollector) {
+            flowCollector.builder()
+        }
+    }
+
+    flow.collect { print(it) }
+    println()
+    flow.collect { print(it) }
+}
+
+// 123
+// 123
