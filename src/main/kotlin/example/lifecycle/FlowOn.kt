8bc26ffd23fa23d9862c683785c1fb69aea79f98
Bandal <no1msh1217@gmail.com>
2025-05-15 00:41:22 +0900

    feat: flowOn 예제 구현

---
+++ src/main/kotlin/example/lifecycle/FlowOn.kt
@@ -0,0 +1,31 @@
+package example.lifecycle
+
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.coroutineContext
+
+suspend fun present(place: String, message: String) {
+    val ctx = coroutineContext
+    val name = ctx[CoroutineName]?.name
+    println("Context: $name | ${place}에서 온 $message  ")
+}
+
+fun messagesFlow(): Flow<String> = flow {
+    present("flow builder", "Message")
+    emit("Message")
+}
+
+suspend fun main() {
+    val users = messagesFlow()
+    withContext(CoroutineName("Name1")) {
+        users
+            .flowOn(CoroutineName("Name3"))
+            .onEach { present("onEach", it) }
+            .flowOn(CoroutineName("Name2"))
+            .collect { present("collect", it) }
+    }
+}
